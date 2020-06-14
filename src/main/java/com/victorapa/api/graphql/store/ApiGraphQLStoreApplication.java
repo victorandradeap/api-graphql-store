de0209cecc39bc6bdc358dc6e4b3a322bf99fe71
victorapa <victorandradeap@gmail.com>
2020-06-13 21:24:13 -0300

    Create project

---
+++ src/main/java/com/victorapa/api/graphql/store/ApiGraphQLStoreApplication.java
@@ -0,0 +1,13 @@
+package com.victorapa.api.graphql.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGraphQLStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGraphQLStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victorapa/api/graphql/store/ApiGraphQLStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorapa.api.graphql.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGraphQLStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
